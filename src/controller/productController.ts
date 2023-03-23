42ee2a8846c7c3ea675cae33b11880f5412b7326
andrebilert <abilert@gmail.com>
2023-03-23 01:01:41 -0300

    Mudando o nome das funcoes do controller

--- src/controller/productController.ts
+++ src/controller/productController.ts
@@ -3,24 +3,24 @@ import ProductService from "../service/productService";
 
 // controller pega o parametro e passa para a service
 class ProductController{
-    async create(req: Request, res: Response){
+    async postCreate(req: Request, res: Response){
        //escrever o body da requisição 
        await ProductService.createProduct(req.body)
 
        return res.status(201).send()
     }
 //controller para buscar e retornar valores 
-    async find(req: Request, res: Response){
+    async readProduct(req: Request, res: Response){
         const produtos = await ProductService.readProduct()
         return res.status(200).send(produtos)
      }
 
-     async stock(req: Request, res: Response){
+     async getStock(req: Request, res: Response){
       const produtos = await ProductService.stockProduct()
       return res.status(200).send(produtos)
      }
 
-     async stockSum(req: Request, res: Response){
+     async getStockSum(req: Request, res: Response){
       const stockValue = await ProductService.getStockValue()
       return res.status(200).json(stockValue)
      }
